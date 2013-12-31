d24b651e2751d8d7c163f24e7d9da4c852b6abe9
Ngo Anh Tuan <ngo.tuan12@gmail.com>
2013-12-31 14:35:27 +0700

    create new web app

--- webapp/eAlarm/src/util/AlarmServiceMethod.dart
+++
@@ -1,7 +0,0 @@
-part of EASUtil;
-class AlarmServiceMethod
-{
-	static final String POST = "POST";
-	static final String GET = "GET";
-	static final String DELETE = "DELETE";
-}
--- webapp/eAlarm/src/util/AlarmServiceName.dart
+++
@@ -1,6 +0,0 @@
-part of EASUtil;
-class AlarmServiceName
-{
-	static final String AreaService = "AreaService";
-	static final String DeviceService = "DeviceServices";
-}
