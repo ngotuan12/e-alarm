a8edd261fb32ae0f97c87af001d7c4cb32f78fb7
ducnd106 <ducnd.eas@gmail.com>
2014-02-11 17:38:27 +0700

    commit function gateway

--- eAlarmWebapp/web/layout/form_gateway.dart
+++ eAlarmWebapp/web/layout/form_gateway.dart
@@ -1,6 +1,5 @@
 import 'package:polymer/polymer.dart';
 import 'dart:html';
-import '../src/util.dart';
 @CustomTag('form-gateway')
 class FormGateway extends PolymerElement
 {
@@ -8,88 +7,30 @@ class FormGateway extends PolymerElement
   // of this element.
   bool get applyAuthorStyles => true;
   FormGateway.created() : super.created();
-  int GatewayID=0;
-  Element tblGateway;
+	
+	@observable String GetcurrentAction;
+	@observable Map currentGateway;
+	@observable String currentAction = "NONE";
+	
   enteredView() 
   {
     super.enteredView();
-    try
-    {
-      
-      tblGateway = this.shadowRoot.querySelector("#ListGateway");
-      Responder responder = new Responder();
-      Map request = new Map();
-      request["Method"] = "GetListGateway";
-      
-      responder.onSuccess.listen((Map response)
-          {
-        List<Map> listGateway=response['ListGateway'];
-        for(int i=0;i<listGateway.length;i++)
-        {
-          Map gateway = listGateway[i];
-          //row
-          TableRowElement row = new TableRowElement();
-          row.classes.add("selectable");
-          
-          //column
-          TableCellElement colId=new TableCellElement();
-          colId.classes.add("center");
-          colId.appendHtml((i+1).toString());
-          
-          TableCellElement colMac=new TableCellElement();
-          colMac.classes.add("center");
-          colMac.appendHtml(gateway["mac_add"].toString());
-          
-          TableCellElement colConnServer=new TableCellElement();
-          colConnServer.classes.add("center");
-          colConnServer.appendHtml(gateway["connected_server"].toString());
-          
-          TableCellElement colStatus=new TableCellElement();
-          colStatus.classes.add("center");
-          if(gateway["connected_server"].toString()=="1")
-          colStatus.appendHtml("OK");
-          else
-            colStatus.appendHtml("Not OK");
-          
-          TableCellElement colAction=new TableCellElement();
-          colAction.classes.add("center");
-          colAction.appendHtml("<button id='' class='btn-action glyphicons remove_2 btn-danger'><i></i></button>");
-          colAction.
-          //add column
-          row.children.add(colId);
-          row.children.add(colMac);
-          row.children.add(colConnServer);
-          row.children.add(colStatus);
-          row.children.add(colAction);
-          GatewayID=gateway["id"];
-          //event click of row item
-          row.onClick.listen((event)=>SendGatewayRequest(gateway["mac_add"].toString(),gateway["connected_server"].toString()));
-          //add row
-          tblGateway.children.add(row);
-        }
-          });
-      //error
-      responder.onError.listen((Map error)
-          {
-        Util.showNotifyError(error["message"]);
-          });
-      AppClient.sendMessage(request, AlarmServiceName.GatewayService, AlarmServiceMethod.POST,responder);
-      
-    }
-    catch(err)
-    {
-      Util.showNotifyError(err.toString());
-    }
   }
-  void SendGatewayRequest(String Mac_Add,String Conn_Ser)
-  {
-    if(Conn_Ser!="null")
-    {
-      
-    }
-    else
-    {
-      
-    }
-  }
+	
+	void onEdit(CustomEvent event)
+	{
+		currentGateway = toObservable(event.detail);
+		currentAction = "EDIT";
+	}
+	
+	void onAdd(CustomEvent event)
+	{
+		currentGateway = null;
+		currentAction = "EDIT";
+	}
+	
+	void onGoBack(CustomEvent event)
+	{
+		currentAction = "NONE";
+	}
 }
