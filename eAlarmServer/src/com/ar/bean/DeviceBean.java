d78a8a41938bb1ef8d102a5e73e853f7eaa7284a
diennd <ducdienpt@gmail.com>
2014-03-05 17:08:45 +0700

    update DeviceBean

--- eAlarmServer/src/com/ar/bean/DeviceBean.java
+++ eAlarmServer/src/com/ar/bean/DeviceBean.java
@@ -26,7 +26,7 @@ public class DeviceBean extends AppProcessor
 			// open connection
 			open();
 			strSQL = "SELECT a.id,a.status,b.name,b.code,b.symbol,b.type,a.value, "
-					+ "b.min_alarm,b.max_alarm,b.`min`,b.`max` "
+					+ "b.min_alarm,b.max_alarm,b.`min`,b.`max`,  "
 					+ "(CASE WHEN a.value<= b.min_alarm or a.value>=b.max_alarm THEN '0' "
 					+ "ELSE '1' end) alarm_status "
 					+ "FROM device_infor a,device_properties b "
